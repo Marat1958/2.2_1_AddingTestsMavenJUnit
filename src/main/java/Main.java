7022df825edab70882c8d47a23f869dfdc4eca44
Marat Safiyulin <marat.safiyulin@mail.ru>
2022-05-03 21:45:17 +0300

    Add two new tests for UnRegistered

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
         BonusService service = new BonusService();
 
         long amount = 1000_60;
-        boolean registered = true;
+        boolean registered = false;
 
         service.calculate(amount, registered);
     }
--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -33,4 +33,37 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000_60;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+
+    }
 }
